fb486878df3e5f6ec4e3cacd7b50356dcf0d2208
ha-yeon-lee <hylee@uds.ai>
2022-01-19 14:47:53 +0900

    [DESIGN] modify header

--- src/section/AppHeader.tsx
+++ src/section/AppHeader.tsx
@@ -9,11 +9,30 @@ const StyledHeader = styled("div")(() => ({
   position: "fixed",
   top: 0,
   left: 0,
-  backgroundColor: "thistle",
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  backdropFilter: "blur(5px)",
+  backgroundColor: "rgba(210, 180, 210, 0.4)",
+  ".title": {
+    paddingLeft: "20px",
+    fontFamily: "DungGeunMo",
+    fontSize: "2rem",
+    color: "white",
+    textShadow: "2px 2px 3px #AAA",
+  },
+  ".menu": {
+    paddingRight: "20px",
+  },
 }));
 
 const AppHeader = () => {
-  return <StyledHeader>Header 영역</StyledHeader>;
+  return (
+    <StyledHeader>
+      <div className="title">useHana();</div>
+      <div className="menu">목록 들어갈 자리</div>
+    </StyledHeader>
+  );
 };
 
 export default AppHeader;
